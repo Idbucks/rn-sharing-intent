75c20b339d67dd627c6fa1fa8117ca5faa80587e
Guillaume G <guillaume@idbucks.com>
2023-07-08 09:17:49 +0200

    feat: first version

---
+++ android/src/main/java/com/rnsharingintent/RnSharingIntentModule.java
@@ -0,0 +1,60 @@
+package com.reactnativereceivesharingintent;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.Intent;
+
+
+import android.os.Build;
+import androidx.annotation.RequiresApi;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+
+public class ReceiveSharingIntentModule extends ReactContextBaseJavaModule {
+  public final String Log_Tag = "ReceiveSharingIntent";
+
+  private final ReactApplicationContext reactContext;
+  private RNSharingIntentHelper rnSharingIntentHelper;
+
+  public ReceiveSharingIntentModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+    this.reactContext = reactContext;
+    Application applicationContext = (Application) reactContext.getApplicationContext();
+    rnSharingIntentHelper = new RNSharingIntentHelper(applicationContext);
+  }
+
+
+  protected void onNewIntent(Intent intent) {
+    Activity mActivity = getCurrentActivity();
+    if(mActivity == null) { return; }
+    mActivity.setIntent(intent);
+  }
+
+  @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+  @ReactMethod
+  public void getFileNames(Promise promise){
+    Activity mActivity = getCurrentActivity();
+    if(mActivity == null) { return; }
+    Intent intent = mActivity.getIntent();
+    rnSharingIntentHelper.sendFileNames(reactContext, intent, promise);
+  }
+
+  @ReactMethod
+  public void clearFileNames(){
+    Activity mActivity = getCurrentActivity();
+    if(mActivity == null) { return; }
+    Intent intent = mActivity.getIntent();
+    if (intent != null) {
+      rnSharingIntentHelper.clearFileNames(intent);
+    }
+  }
+
+
+  @Override
+  public String getName() {
+    return "ReceiveSharingIntent";
+  }
+}
