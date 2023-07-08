a458161bd94e7ea57d3e3b096c7ab114caff8c5e
Guillaume G <guillaume@idbucks.com>
2023-07-08 15:34:05 +0200

    fix: try fix ios

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-rn-sharing-intent';
+import RNShareIntent from 'rn-sharing-intent';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
 
   React.useEffect(() => {
-    multiply(3, 7).then(setResult);
+    RNShareIntent.clearFileNames();
   }, []);
 
   return (
