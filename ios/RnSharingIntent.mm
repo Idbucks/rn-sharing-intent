1c52045ce7bdb654679f824075e2698e52f798f3
Guillaume G <guillaume@idbucks.com>
2023-07-08 16:16:45 +0200

    fix: mm change

--- ios/RnSharingIntent.mm
+++ ios/RnSharingIntent.mm
@@ -2,9 +2,11 @@
 
 @interface RCT_EXTERN_MODULE(RnSharingIntent, NSObject)
 
-RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
-                 withResolver:(RCTPromiseResolveBlock)resolve
-                 withRejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(getFileNames:(NSString)url
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter: (RCTPromiseRejectBlock)reject);
+
+RCT_EXTERN_METHOD(clearFileNames)
 
 + (BOOL)requiresMainQueueSetup
 {
